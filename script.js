590fb07793d45bbd986629c5b767fda95a2e9244
Djeague Prince Dupont <dupontdjeague@gmail.com>
2026-04-23 06:37:15 +0100

    udate script -b main

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault()
   alert('Formulaire envoyé !')
 })
+document.querySelector('input[type="text"]').addEventListener('input', (e) => {
+  console.log('Texte saisi :', e.target.value)
+})
+document
+  .querySelector('input[type="checkbox"]')
+  .addEventListener('change', (e) => {
+    console.log('Case à cocher :', e.target.checked)
+  })
